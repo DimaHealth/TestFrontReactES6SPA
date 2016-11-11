f92b11a8d37f029aeb6b22dbb8530439fa777276
Denis Sych <sdv_box@hotmail.com>
2016-11-11 14:31:22 +0200

    Попытка собрать все вместе

--- static/app.js
+++ static/app.js
@@ -1,41 +1,49 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
 
+import withExampleBasename from '../withExampleBasename'
 
-class Form extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-      value: '',
-      message: []
-    };
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
+const ACTIVE = { color: 'red' }
 
-	handleChange() {
-		this.setState({value: event.target.value});
-	}
-	handleSubmit(event) {
-    let {message, value} = this.state;
-	message.push(value);
-	this.setState({message, value: ''});
-}
-	render() {
-		return (
-		<div>
-		<textarea className="input-form" rows={6} cols={35}  type = "text" placeholder = "Вводите текст" value={this.state.value}
-		onChange={this.handleChange}/>
-		<button className="go" onClick={this.handleSubmit}>
-		Поехали
-		</button>
-		{this.state.message.map((message, id) => <p id={id} className="messageIn">{message}</p>)}
+const App = ({ children }) => (
+  <div>
+    <h1>APP!</h1>
+    <ul className="nav nav-tabs">
+      <li><IndexLink     to="/"            activeStyle={ACTIVE}>Список заявок</IndexLink></li>
+      <li><Link          to="/chat"        activeStyle={ACTIVE}>Чат</Link></li>
+      <li><Link          to="/newbid"      activeStyle={ACTIVE}>Новая заявка</Link></li>
+    </ul>
 
-		</div>
+    {children}
+  </div>
+)
 
-		);
-	}
-}
-ReactDOM.render(
-<Form />, document.getElementById('send-message_form'));
-//export default Form;
+const Listofbids = () => (
+  <div>
+    <h2>Заявки</h2>
+  </div>
+)
+
+const Chat = () => (
+  <div>
+    <h2>Чат</h2>
+</div>
+)
+
+const Newbid = () => (
+  <div>
+    <h2>Новая заявка</h2>
+  </div>
+)
+
+render((
+  <Router history={withExampleBasename(browserHistory, __dirname)}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Listofbids}/>
+      <Route path="/newbid" component={Newbid}/>
+      <Route path="chat" component={Chat}>
+      </Route>
+    </Route>
+  </Router>
+), document.getElementById('example'))
