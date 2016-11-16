99cd639010f7d4bda09a8779590d8105b3c2bc16
Denis Sych <sdv_box@hotmail.com>
2016-11-16 08:32:46 +0200

    Add CalcInput

---
+++ static/modules/calcinput.jsx
@@ -0,0 +1,36 @@
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(value, convert) {
+  const input = parseFloat(value);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+class CalcInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+
+  render() {
+    const value = this.props.value;
+    return (
+        <input value={value}
+               onChange={this.handleChange} />
+    );
+  }
+}
