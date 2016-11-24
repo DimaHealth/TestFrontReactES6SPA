b566859e79459635ec1e1be5e488e477a7dd5392
Denis Sych <Denis Sych>
2016-11-24 08:46:34 +0200

    qwe

--- static/modules/Newbidrc/markinfo.jsx
+++ static/modules/Newbidrc/markinfo.jsx
@@ -22,29 +22,58 @@ import ReactDOM from 'react-dom'
 class Markinfo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {data: [],
-                      selected:''
-                     };
+        this.state = {value: ''};
         this.handleChange = this.handleChange.bind(this);
         // this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
 //        console.log(e.target.value);
- //       this.setState({value: e.target.value});
+        this.setState({value: e.target.value});
         this.props.onFuck(Number(e.target.value));
     }
     
     render() {
         if(this.props.options!=null){
-        var options = this.props.options.map(function(option, key) {
+          var options = this.props.options.map((option, key) => {
             return (
                 <option key={key} value={option.id}>{option.name}</option>
             )
-        })};
+          })
+        }
+        else if (this.props.years!=null){
+          let year=this.props.years[0];
+          let y;
+          if (this.props.years[1]!=9999){
+            y=this.props.years[1]-year;
+          }
+          else{
+            let d = new Date();
+            y=d.getFullYear()-year;
+          };
+          var options=[];
+          var years=[];
+          for ( let x=0; x<=y; x++ ){years[x]=year;year++};
+          var options = years.map((year, key) => {
+            return (
+                <option key={key} value={year}>{year}</option>
+            )
+          })
+        //   do{
+        //     //   options[x]='<option key=' + x + '>' + year + '</option>';
+        //   options[x] = function(option) {
+        //     return (
+        //         <option key={x} value={year}>{year}</option>
+        //     )
+        //   }
+        //       x++;
+        //       year++;
+        //   } while (year<=this.props.years[1])
+        console.log(options);
+        };
         return (
                             <select onChange={this.handleChange}>
-                              <option>{this.props.placeholder}</option>  
+                              <option selected value={this.props.placeholder}>{this.props.placeholder}</option>  
                               {options}
                             </select>
         );
--- static/modules/Newbidrc/newbidcolright.jsx
+++ static/modules/Newbidrc/newbidcolright.jsx
@@ -11,17 +11,7 @@ import Uploadphoto from './uploadphoto'
  class Bidcolright extends React.Component {
      constructor(props) {
          super(props);
-        this.state = {
-            auto:{
-                modelId: '',
-                markId: '',
-                year:''
-            },
-            message:'',
-            messageImg:'',
-            desiredDate:''
-
-        };
+        this.state = { auto:{modelId: null, markId: null, year: null} };
         this.onMarkSelect = this.onMarkSelect.bind(this);
         this.onModelSelect = this.onModelSelect.bind(this);
         this.onYearSelect = this.onYearSelect.bind(this);
@@ -29,17 +19,26 @@ import Uploadphoto from './uploadphoto'
     }
 
     onMarkSelect(e) {
-        let auto = this.state.auto;
-        auto.markId=e;
-        this.setState({ auto });
-        $.getJSON('./static/json/'+ e + '.json', function(data) {this.setState({model: data})}.bind(this));
+          let auto = this.state.auto;
+          auto.markId=e;
+          auto.modelId=null;
+          auto.year=null;
+          this.setState({ auto, modelyears: [], models: [] });
+          // $.getJSON('./static/json/'+ e + '.json', function(data) {this.setState({model: data})}.bind(this));
+        if(e){
+          $.getJSON('https://test.uremont.com/auto/models?mark='+ e , function(data) {this.setState({models: data})}.bind(this));
+        }
     }
 
       onModelSelect(e) {
         let auto = this.state.auto;
          auto.modelId=e;
-         this.setState({ auto });
-        $.getJSON('./static/json/'+ e + '.json', function(data) {this.setState({modelyear: data})}.bind(this));
+         auto.year=null;
+         this.setState({ auto, modelyears: [] });
+        // $.getJSON('./static/json/'+ e + '.json', function(data) {this.setState({modelyear: data})}.bind(this));
+        if(e){
+        $.getJSON('https://test.uremont.com/auto/year?model='+ e, function(data) {this.setState({modelyears: data})}.bind(this));
+        }
     }
 
    onYearSelect(e) {
@@ -53,7 +52,7 @@ import Uploadphoto from './uploadphoto'
         this.setState({ message, value: '' });
     }
     componentDidMount(){
-      $.getJSON('./static/json/auto brands.json', function(data) {this.setState({data: data})}.bind(this));
+      $.getJSON('./static/json/auto brands.json', function(data) {this.setState({marks: data})}.bind(this));
     }
     render() {
 //      $.getJSON('./static/json/auto brands.json', function(data) {this.setState({data: data})}.bind(this));
@@ -61,9 +60,9 @@ import Uploadphoto from './uploadphoto'
         return (
             <div className="rightColumn">
                 <div className="rightColumn-markModYear">
-                    <Markinfo options = {this.state.data} value={this.state.message} onFuck={this.onMarkSelect} placeholder='Выберите марку'/>
-                    <Markinfo options = {this.state.model} value={this.state.message} onFuck={this.onModelSelect} placeholder='Выберите модель'/>
-                    <Markinfo options = {this.state.modelyear} value={this.state.message} onFuck={this.onYearSelect} placeholder='Выберите год'/>
+                    <Markinfo options = {this.state.marks} onFuck={this.onMarkSelect} placeholder='Марка'/>
+                    <Markinfo options = {this.state.models} onFuck={this.onModelSelect} placeholder='Модель'/>
+                    <Markinfo years = {this.state.modelyears} onFuck={this.onYearSelect} placeholder='Год'/>
                 </div>
 
 
