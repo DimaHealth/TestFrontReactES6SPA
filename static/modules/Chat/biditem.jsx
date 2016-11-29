66c0f514d6baf33daecb3840849c73ce87b4135d
Denis Sych <Denis Sych>
2016-11-29 17:27:48 +0200

    Bids - начало.

--- static/modules/Chat/biditem.jsx
+++ static/modules/Chat/biditem.jsx
@@ -6,16 +6,8 @@ class BidItem extends React.Component {
         return (
             <div>
             <div className="tabs-first tab-styling">
-                <p> <span className="orderNumber">№4491</span> Chevrolet Avalanche <span className="dateValue">25.10.2016 </span> <span className="vinNumber">VIN не указан</span></p>
-                <p className = "bidWriting"> ybxt </p>
-            </div>
-            <div className = "tabs-second tab-styling ">
-                <p> <span className="orderNumber">№5000</span> Chevrolet Avalanche <span className="dateValue">30.12.2016 </span> <span className="vinNumber">BY43WQ12BC54KK321</span></p>
-                <p className = "bidWriting"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores consequatur  </p>
-            </div>
-            <div className = "tabs-third tab-styling">
-                <p> <span className="orderNumber">№5000</span> Bentley Continental Supersports <span className="dateValue">30.12.2016 </span> <span className="vinNumber">BY43WQ12BC54KK321</span></p>
-                <p className = "bidWriting"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores consequatur adipisicing elit. Dolores consequatur </p>
+                <p> <span className="orderNumber">{this.props.bid.bid_id}</span>{' ' + this.props.bid.car.brand_name +' '+ this.props.bid.car.model_name}<span className="dateValue">25.10.2016 </span> <span className="vinNumber">VIN не указан</span></p>
+                <p className = "bidWriting"> {this.props.bid.description} </p>
             </div>
             </div>
         );
--- static/modules/ChatApp.jsx
+++ static/modules/ChatApp.jsx
@@ -11,9 +11,14 @@ require('./chat/scss/chat.scss');
 class ChatApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { messages: [] };
+        this.state = { messages: [], bids: null };
 //        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    componentDidMount(){
+      $.getJSON('./static/json/bids.json', function(data) {this.setState({bids: data})}.bind(this));
 
     }
 
@@ -22,10 +27,13 @@ class ChatApp extends React.Component {
     }
 
 	render() {
+    if (this.state.bids){
+
+    var bidItems = this.state.bids.map((bid, key) => {return <BidItem bid={bid} key={key}/>});
 		return (
     <div className="carcas">
       <div className="left-column_tabs">
-        <BidItem />
+        {bidItems}
       </div>
       <div className = "right-column_chat">
         <Infotable />
@@ -40,6 +48,26 @@ class ChatApp extends React.Component {
       </div>
     </div>
 		);
+    } else {
+      return (
+    <div className="carcas">
+      <div className="left-column_tabs">
+      </div>
+      <div className = "right-column_chat">
+        <Infotable />
+        <div className="chat-window">
+          <p className="entry-message"> <span> Оганнес </span><br/>Привет родной, как дела? </p>
+            <Messages messages={this.state.messages} className="message-block_example"/>
+        </div>
+        <div className="calculator-and-chatTextarea">
+          <Calculator />
+          <Chatform  onSubmit={this.handleSubmit}/>
+        </div>
+      </div>
+    </div>
+        
+      )
+    }
 	}
 }
 
