15046cbf7fb74a441036bf14078e064012460c12
tigran.raider@mail.ru <tigran.raider@mail.ru>
2016-11-29 15:01:04 +0300

    Мелкие правки

--- static/modules/Chat/tabs.jsx
+++ static/modules/Chat/tabs.jsx
@@ -7,15 +7,15 @@ class Tabs extends React.Component {
             <div className="left-column_tabs">
             <div className="tabs-first tab-styling">
                 <p> <span className="orderNumber">№4491</span> Chevrolet Avalanche <span className="dateValue">25.10.2016 </span> <span className="vinNumber">VIN не указан</span></p>
-                <p className = "messageText"> ybxt </p>
+                <p className = "bidWriting"> ybxt </p>
             </div>
             <div className = "tabs-second tab-styling ">
                 <p> <span className="orderNumber">№5000</span> Chevrolet Avalanche <span className="dateValue">30.12.2016 </span> <span className="vinNumber">BY43WQ12BC54KK321</span></p>
-                <p className = "messageText"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores consequatur  </p>
+                <p className = "bidWriting"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores consequatur  </p>
             </div>
             <div className = "tabs-third tab-styling">
                 <p> <span className="orderNumber">№5000</span> Bentley Continental Supersports <span className="dateValue">30.12.2016 </span> <span className="vinNumber">BY43WQ12BC54KK321</span></p>
-                <p className = "messageText"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores consequatur adipisicing elit. Dolores consequatur </p>
+                <p className = "bidWriting"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores consequatur adipisicing elit. Dolores consequatur </p>
             </div>
         </div>
         );
