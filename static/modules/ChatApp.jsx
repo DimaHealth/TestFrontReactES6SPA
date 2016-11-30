644445b8e5f640855e65da2079b99a47a8ac9ac2
Denis Sych <Denis Sych>
2016-11-30 13:26:40 +0200

    BidItem - смена стиля по клику

--- static/modules/ChatApp.jsx
+++ static/modules/ChatApp.jsx
@@ -11,9 +11,10 @@ require('./chat/scss/chat.scss');
 class ChatApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { messages: [], bids: null };
+        this.state = { messages: [], bids: null, selected: null };
 //        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onBidItemSelect = this.onBidItemSelect.bind(this);
 
     }
 
@@ -26,10 +27,24 @@ class ChatApp extends React.Component {
        this.setState({messages: e});
     }
 
+    onBidItemSelect(e) {
+      this.setState({selected: e});
+    }
+
 	render() {
     if (this.state.bids){
+    //   console.log(this.state.bids);
 
-    var bidItems = this.state.bids.map((bid, key) => {return <BidItem bid={bid} key={key}/>});
+    // var bidItems = this.state.bids.map((bid, key) => {return <BidItem bid={bid} key={key} onClick={this.onBidItemSelect} style = "tab-styling_selected"/>});
+          
+
+    var bidItems = this.state.bids.map((bid, key) => {
+      if (bid.bid_id==this.state.selected){
+        return  <BidItem bid={bid} key={key} onClick={this.onBidItemSelect} style = 'tab-styling_selected'/>  
+      } else {
+        return  <BidItem bid={bid} key={key} onClick={this.onBidItemSelect} style = 'tab-styling'/>
+      }});
+      
 		return (
     <div className="carcas">
       <div className="left-column_tabs">
