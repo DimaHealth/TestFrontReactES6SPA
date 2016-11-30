d3206937771d9062042cb3201d611810dc1b0ae8
Denis Sych <Denis Sych>
2016-11-30 10:47:24 +0200

    BidItem - continue

--- static/modules/Chat/biditem.jsx
+++ static/modules/Chat/biditem.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 
 class BidItem extends React.Component {
     render() {
+        var date = new Date(this.props.bid.current_time*1000);
+//        console.log(dat);
         return (
             <div>
             <div className="tabs-first tab-styling">
-                <p> <span className="orderNumber">{this.props.bid.bid_id}</span>{' ' + this.props.bid.car.brand_name +' '+ this.props.bid.car.model_name}<span className="dateValue">25.10.2016 </span> <span className="vinNumber">VIN не указан</span></p>
+                <p> <span className="orderNumber">{this.props.bid.bid_id}</span>{' ' + this.props.bid.car.brand_name +' '+ this.props.bid.car.model_name}<span className="dateValue">{date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()}</span></p>
                 <p className = "bidWriting"> {this.props.bid.description} </p>
             </div>
             </div>
